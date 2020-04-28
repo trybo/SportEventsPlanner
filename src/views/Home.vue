e868221a98793dbdac67f8974be58d4dab6fb66a
Pawel Czachura <trybo1234@gmail.com>
2020-04-28 15:13:22 +0200

    Added Cards to Home in component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="overflow-hidden">
     <Navbar />
-    <h1>home</h1>
+    <HomeCards />
     <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import HomeCards from '@/components/HomeCards'
 import Footer from '@/components/Footer'
 
 export default {
     components: {
         Navbar,
+        HomeCards,
         Footer
     }
 }
