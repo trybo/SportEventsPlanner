ef38260faaf017947d30a1e42f887ecec37bc58f
Pawel Czachura <trybo1234@gmail.com>
2020-04-29 15:36:11 +0200

    Added Events View

---
+++ src/views/Events.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home-container" class="overflow-hidden">
+    <Navbar />
+    <EventCard />
+    <Footer />
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Navbar";
+import EventCard from "@/components/EventCard";
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Navbar,
+    EventCard,
+    Footer
+  }
+};
+</script>
+
+<style>
+#home-container {
+  position: relative;
+  min-height: 100vh;
+}
+</style>
