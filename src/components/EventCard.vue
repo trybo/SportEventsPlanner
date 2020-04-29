2e730ab04fc2455766df44a414c58c18e93989e4
Pawel Czachura <trybo1234@gmail.com>
2020-04-29 15:33:59 +0200

    Added card with event component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-deck text-center mx-4 my-5">
+    <div class="card bg-transparent text-white p-2">
+      <div class="card-body">
+        <h3 class="card-title">Card title</h3>
+        <p
+          class="card-text"
+        >This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.card{
+    border: none;
+}
+</style>
